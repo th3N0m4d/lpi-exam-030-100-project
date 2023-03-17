719239fe56a219c02c748ce0aeb72dc543f41497
th3N0m4d <edielton.estrela.dantas@sap.com>
2023-03-17 09:02:15 +0100

    feat: integrate with EJS layouts

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const ejsLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
 // view engine setup
+app.use(ejsLayouts);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -22,7 +23,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // route setup
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
