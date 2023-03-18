2540f7cd6c5874745ffb3a5cab15498488e31d59
th3N0m4d <edielton.estrela.dantas@sap.com>
2023-03-18 09:48:20 +0100

    fix: move header out of the layout page

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const dbConfig = require("../db/config");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   const db = dbConfig.getDB();
-
+  /**@todo abstract away this into a business layer */
   db.all("SELECT * FROM product", [], (err, rows = []) => {
     const products = rows.map(({ id, image_src, name, price }) => ({
       id,
